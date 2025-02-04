f7928ca792e4da5f9f9a48e4b46b873ca2eb53bb
Amene <amene.ahmadi.227@gmail.com>
2025-02-04 15:13:04 +0330

    init

--- amene/src/App.jsx
+++ amene/src/App.jsx
@@ -1,35 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [clickEvent, setClickEvent] = useState("");
 
+  const handleParentClick = () => {
+    setClickEvent((prev) => prev + "Parent Clicked!");
+  };
+
+  const handleChildClick = (e) => {
+    e.stopPropagation();
+    setClickEvent((prev) => prev + "Child Clicked!");
+  };
+  
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="w-full flex justify-center items-center flex-col">
+        <h1 className="mb-5">{clickEvent}</h1>
+        <div
+          onClick={handleParentClick}
+          className="w-80 h-52 bg-sky-500 flex justify-center items-center"
+        >
+          <div
+            onClick={handleChildClick}
+            className="w-52 h-24 bg-pink-300 text-center flex items-center justify-center cursor-pointer"
+          >
+            Click me
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
